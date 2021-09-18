8d6293fcbf98cb8fbd9f51de28002e573d9f7d98
Meghana-Sk <meghana.meghanask.sk@gmail.com>
2021-09-18 17:52:50 +0530

    fix: Added negative values checks for triangles sides and angles

--- anglesofTriangle.js
+++ anglesofTriangle.js
@@ -7,11 +7,18 @@ const result = document.querySelector("#display-result");
 checkbtn.addEventListener("click",checkAnglesOfTriangle);
 
 function checkAnglesOfTriangle() {
-
+if(
+    (Number(angles[0].value) > 0 && Number(angles[0].value) <= 360 )
+     &&  (Number(angles[1].value) > 0 && Number(angles[1].value) <= 360 ) 
+     && (Number(angles[2].value) > 0 && Number(angles[2].value) <= 360 )
+     ) {
     if(Number(angles[0].value) + Number(angles[1].value) + Number(angles[2].value) === 180) 
     {
         result.innerText = "Yay. You've entered angles of a triangle"
     } else {
         result.innerText = "No. Angles you have inputted are not angles of triangle"
     }
+} else {
+result.innerText = "Enter valid angles";
+}
 }
